b111f1fda41b34423c64c4a5f37872c9133af56a
lalit113 <lalit24899@gmail.com>
2022-06-10 09:38:32 +0530

    Initial Commit

---
+++ src/main/java/io/github/lalit113/calculator/App.java
@@ -0,0 +1,13 @@
+package io.github.lalit113.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
