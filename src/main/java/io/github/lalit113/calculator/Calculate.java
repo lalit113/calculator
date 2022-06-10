e98e2c40724b21f678e13f47f8a2e66e725afc4e
lalit113 <lalit24899@gmail.com>
2022-06-10 10:57:56 +0530

    Added two more methods in Calculate.java

--- src/main/java/io/github/lalit113/calculator/Calculate.java
+++ src/main/java/io/github/lalit113/calculator/Calculate.java
@@ -8,4 +8,12 @@ public class Calculate {
 	public int sub(int a, int b) {
 		return a - b;
 	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
 }
